061beaa55d595f9d848b79810b8ec1e57019d3bb
Andrew Steel <dev@andrewsteel.net>
2019-05-13 17:42:01 +0100

    Add bin/populate.sh
    Populates the files directory with configuration files from the current
    system.

---
+++ bin/populate.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# This file is part of andrew-ls/dotfiles.
+#
+# Copyright (c) 2019 Andrew Steel <copyright@andrewsteel.net>
+#
+# SPDX-License-Identifier: GPL-3.0-only
+#
+# The GNU General Public License Version 3 only:
+#
+# This is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License Version 3
+# as published by the Free Software Foundation.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this file. If not, see <https://www.gnu.org/licenses/>.
+
+cd "$(dirname "${0}")" || exit
+
+FILES_ROOT='../files'
